44fe0d3c6a6160452b4ea71bf01008e86259dd0c
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-02-04 00:40:47 +0700

    Create query

---
+++ src/components/BookList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo'
+
+const getBooksQuery = gql`
+  {
+    books {
+      id,
+      name
+    }
+  }
+`;
+
+class BookList extends Component {
+  displayBooks() {
+    const { data } = this.props;
+
+    if (data.loading) {
+      return (
+        <div>Loading books...</div>
+      );
+    }
+
+    return data.books.map(book => <li key={book.id}>{book.name}</li>);
+  }
+
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          {this.displayBooks()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default graphql(getBooksQuery)(BookList);
